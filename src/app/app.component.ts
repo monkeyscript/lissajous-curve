d878c2af0f8d7e1f50544623c11e618d92124c3e
MonkeyScript <monkeyscript.dev@gmail.com>
2020-04-06 22:41:46 +0530

    Added responsive canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {
   Component,
-  OnInit
+  AfterViewInit,
+  ViewChild,
+  ElementRef
 } from '@angular/core';
 
 @Component({
@@ -8,14 +10,14 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
   title = 'lissajous-curve';
 
   // Parameters 
-  A: number = 1;
+  A: number = 10;
   a: number = 1;
-  B: number = 1;
-  b: number = 1;
+  B: number = 10;
+  b: number = 2;
   t: number = 0;
 
   // Result 
@@ -25,9 +27,38 @@ export class AppComponent implements OnInit {
   // Timer 
   timer: any
 
-  ngOnInit() {
+  // Canvas element reference 
+  @ViewChild('canvas', {
+    static: false
+  }) public canvas: ElementRef;
 
-    this.simulate()
+  // Canvas context var
+  private ctx: CanvasRenderingContext2D;
+
+  // Canvas dimension
+  canvasDimension: number;
+
+  ngAfterViewInit() {
+
+    // Get the context 
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.ctx = canvasEl.getContext('2d');
+
+    setTimeout(() => {
+
+      // Get offset width
+      this.canvasDimension = canvasEl.offsetWidth;
+      console.log(this.canvasDimension, canvasEl.width)
+
+      // Update height and width
+      canvasEl.width = this.canvasDimension;
+      canvasEl.height = this.canvasDimension;
+
+      // Start simulation 
+      this.simulate()
+
+    }, 5000);
+
 
   }
 
@@ -38,6 +69,9 @@ export class AppComponent implements OnInit {
     this.x = 0;
     this.y = 0;
 
+    // Clear canvas
+    this.ctx.clearRect(0,0,this.canvasDimension,this.canvasDimension)
+
     // Stop timer 
     clearInterval(this.timer)
 
@@ -47,10 +81,25 @@ export class AppComponent implements OnInit {
       this.x = this.A * Math.sin((this.a * this.t))
       this.y = this.B * Math.sin((this.b * this.t))
 
+      // Mark point 
+      this.strokePoint(this.x, this.y);
+
       // Increement time 
       this.t++;
 
-    }, 2000);
+    }, 100);
+
+  }
+
+  strokePoint(x: number, y: number) {
+
+    var pointSize = 2; // Change according to the size of the point.
+
+    this.ctx.fillStyle = "#ff2626"; // Red color
+
+    this.ctx.beginPath(); //Start path
+    this.ctx.arc((this.canvasDimension / 2) + (x * 10), (this.canvasDimension / 2) - (y * 10), pointSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
+    this.ctx.fill(); // Close the path and fill.
 
   }
 
